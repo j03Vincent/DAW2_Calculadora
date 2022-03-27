b5ed7f2bdf880fb601ced65f3324c7beee837683
Vincent <joevincentg24@gmail.com>
2022-03-27 19:55:30 +0100

    interpolacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DAW2 Calculadora';
+  student = 'Joe Vincent Geronimo';
   currentNumber = '0';
 	operation = '';
 
