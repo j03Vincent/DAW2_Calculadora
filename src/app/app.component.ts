38a3a18dd16a7308ce92c6a28db78fdbdae6855b
Vincent <joevincentg24@gmail.com>
2022-03-27 19:01:52 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DAW2_Calculadora';
+  title = 'DAW2 Calculadora';
+  currentNumber = '0';
+	operation = '';
+
+	constructor() {}
+
+	getOperation(op: string) {
+		this.operation = op;
+		this.currentNumber += op;
+	}
+
+	getNumber(number: string) {
+		if (this.currentNumber === '0') {
+			this.currentNumber = number;
+		} else {
+			this.currentNumber += number;
+		}
+	}
+
+	addDecimalPoint() {
+		this.currentNumber += '.';
+	}
+
+	calc() {
+		let result = eval(this.currentNumber);
+		this.currentNumber = (this.currentNumber.includes('.'))?
+			String(result.toFixed(2)) : String(result);
+	}
+
+	clearAll() {
+		this.currentNumber = '0';
+		this.operation = '';
+	}
 }
